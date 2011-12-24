58d66acb28cef17e5123b5b102cb2f03d929c8b5
René Hansen <renehh@gmail.com>
2011-12-24 02:05:05 +0100

    Solved problem 52.

--- 52.c
+++ 52.c
@@ -5,9 +5,48 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 int main(int argc, const char *argv[])
 {
-  
+  int digits0[10];
+  int digits1[10];
+  unsigned int num = 2;
+  unsigned int tmp0, tmp1;
+  int i, j;
+  int all_match;
+
+  while(1)
+  {
+    tmp0 = num;
+    memset(digits0, 0, sizeof(int) * 10);
+    while(tmp0)
+    {
+      digits0[tmp0 % 10]++;
+      tmp0 /= 10;
+    }
+    all_match = 1;
+    for (i = 2; i <= 6; i++) {
+      tmp1 = num * i;
+      memset(digits1, 0, sizeof(int) * 10);
+      while(tmp1)
+      {
+        digits1[tmp1 % 10]++;
+        tmp1 /= 10;
+      }
+      if(memcmp(digits0, digits1, sizeof(int) * 10) != 0)
+      {
+        all_match = 0;
+        break;
+      }
+    }
+    if(all_match) {
+      printf("Result: %d\n", num);
+      break;
+    }
+    num++;
+  }
+
   return 0;
 }
